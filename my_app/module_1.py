f67be48ef86266bb12fa351a17a58a298f675bfe
Causb1A <79050900+Causb1A@users.noreply.github.com>
2022-04-23 16:52:14 +0100

    comments update

--- my_app/module_1.py
+++ my_app/module_1.py
@@ -22,20 +22,19 @@ def does_something():
 
 
 def run():
-    # This should output some logs but Not the debug mode
+    # This should output some logs but Not the debug mode only INFO
     does_something()
     # lets try module 2
     does_something_module_2()
-    # still no debug right?
+    # still no debug - ONLY INFO
 
-    # Now I'm going to set debug mode to be true
+    # Now I'm going to set debug mode to be true - FUNCTION THAT CHANGES THE SINGLETON CLASS AND LOG
     function_initiates_debug_mode()
 
-    # Let's run the function does_something() again to see that debug will now be outputted
+    # Let's run the function does_something() again to see that debug will now be outputted. Debug is outputted here, great this is what I want
     does_something()
 
-    # debug is now outputted right?
-    # how about module 2
+    # how about module 2 - DEBUG IS NOT OUTPUTTED HERE. I want this outputted with DEBUG.
     does_something_module_2()
 
     # Debug mode is created with one class singleton instance, you can change the variables on the fly. initialising debug mode.
